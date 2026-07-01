4cdcba30b2d613a94b186c97d760d17ceec41eb6
jlfajardo33 <josefajardoromero@gmail.com>
2026-06-30 22:04:42 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let invitados=[];
 fetch('Invitados - Mesa.xlsx').then(r=>r.arrayBuffer()).then(b=>{
  const wb=XLSX.read(b); const ws=wb.Sheets[wb.SheetNames[0]];
  const data=XLSX.utils.sheet_to_json(ws);
+ console.log(data);
  invitados=data;
 });
 const inp=document.getElementById('buscar');
